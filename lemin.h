a686345e773b41abe8fc8a957d9a1dd0191502bf
npiatiko <piatikop@gmail.com>
2019-02-11 18:37:04 +0200

    parsing first steps

---
+++ lemin.h
@@ -0,0 +1,14 @@
+#ifndef LEMIN_H
+#define LEMIN_H
+
+#include "./libft/libft.h"
+
+typedef struct		s_room
+{
+	char			*name;
+	char			type; /*s-start, e-end, n-normal*/
+	struct s_room	*link;
+	struct s_room	*next;
+}					t_room;
+
+#endif
--- main.c
+++ main.c
@@ -1,7 +1,46 @@
-#include <stdio.h>
+#include "lemin.h"
 
+int ft_isnbr(char *str)
+{
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (0);
+		str++;
+	}
+	return 1;
+}
 int main()
 {
-	printf("Hello, World!\n");
+//	ft_printf("%.20f", 9.2);
+
+	char **words;
+	char *line;
+	int ants;
+	char typeroom;
+
+	ants = 0;
+	typeroom = 'n';
+	while (get_next_line(0, &line) > 0)
+	{
+		if (ft_isnbr(line))
+		{
+			ants = ft_atoi(line);
+			free(line);
+			continue;
+		}
+		if (line[0] == '#' && line[1] != '#')
+		{
+			free(line);
+			continue;
+		}
+		if (ft_strncmp(line, "##", 2))
+		{
+			typeroom = (char)(ft_strequ(line, "##start") ? 's' : 'n');
+			typeroom = (char)(ft_strequ(line, "##end") ? 'e' : typeroom);
+			free(line);
+			continue;
+		}
+	}
 	return 0;
 }
