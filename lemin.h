3c72ef90a5c0064e89d7a9130440c4c500d1b5ff
npiatiko <piatikop@gmail.com>
2019-02-12 17:06:47 +0200

    Parse pharm

--- lemin.h
+++ lemin.h
@@ -7,8 +7,11 @@ typedef struct		s_room
 {
 	char			*name;
 	char			type; /*s-start, e-end, n-normal*/
+	int				x;
+	int				y;
 	struct s_room	*link;
 	struct s_room	*next;
 }					t_room;
+int					g_ants;
 
 #endif
--- main.c
+++ main.c
@@ -1,5 +1,31 @@
 #include "lemin.h"
 
+t_room	*ft_new_room(char **data, char typeroom)
+{
+	t_room	*new;
+
+	new = (t_room *)ft_memalloc(sizeof(t_room));
+	new->name = ft_strdup(data[0]);
+	new->x = ft_atoi(data[1]);
+	new->y = ft_atoi(data[2]);
+	new->type = typeroom;
+	return (new);
+}
+
+void	ft_room_add(t_room **head, t_room *new)
+{
+	new->next = *head;
+	*head = new;
+}
+
+void ft_print_graph(t_room *head)
+{
+	while (head)
+	{
+		ft_printf("%s, %d, %d, %c\n", head->name, head->x, head->y, head->type);
+		head = head->next;
+	}
+}
 int ft_isnbr(char *str)
 {
 	while (*str)
@@ -14,33 +40,50 @@ int main()
 {
 //	ft_printf("%.20f", 9.2);
 
-	char **words;
-	char *line;
-	int ants;
-	char typeroom;
+	char	**words;
+	char	*line;
+	char	typeroom;
+	t_room	*graph;
 
-	ants = 0;
+	graph = NULL;
+	g_ants = 0;
 	typeroom = 'n';
+
 	while (get_next_line(0, &line) > 0)
 	{
+
 		if (ft_isnbr(line))
 		{
-			ants = ft_atoi(line);
+			g_ants = ft_atoi(line);
 			free(line);
 			continue;
 		}
+
 		if (line[0] == '#' && line[1] != '#')
 		{
 			free(line);
 			continue;
 		}
-		if (ft_strncmp(line, "##", 2))
+
+		if (!ft_strncmp(line, "##", 2))
 		{
-			typeroom = (char)(ft_strequ(line, "##start") ? 's' : 'n');
-			typeroom = (char)(ft_strequ(line, "##end") ? 'e' : typeroom);
+
+			typeroom = (char)(ft_strequ(line, "##start") ? 's' : typeroom);
+			typeroom = (char)(ft_strequ(line, "##end") ? 'e' : 'n');
 			free(line);
 			continue;
 		}
+
+		if (ft_strchr(line, ' '))
+		{
+			words = ft_strsplit(line, ' ');
+			ft_room_add(&graph, ft_new_room(words, typeroom));
+			free(words);
+		}
+//		ft_printf("%s\n", line);
+
 	}
+//	ft_printf("%d\n", g_ants);
+	ft_print_graph(graph);
 	return 0;
 }
